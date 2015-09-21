be4c210b15343cb8724c17cee76814db881f733d
Luke Patrick <luke@snodandjeff.com>
2015-09-21 04:16:34 +0000

    Moves some of the widget areas and the custom elements to new lib files

--- functions.php
+++ functions.php
@@ -42,70 +42,11 @@ require_once('lib/presstrends.php'); // load PressTrends to track the usage of R
 */
 require_once('lib/custom-o-matic.php');
 
-/**********************
-Add theme supports
- **********************/
-if( ! function_exists( 'reverie_theme_support' ) ) {
-    function reverie_theme_support() {
-        // Add language supports.
-        load_theme_textdomain('reverie', get_template_directory() . '/lang');
-
-        // Add post thumbnail supports. http://codex.wordpress.org/Post_Thumbnails
-        add_theme_support('post-thumbnails');
-        // set_post_thumbnail_size(150, 150, false);
-        add_image_size('fd-lrg', 1024, 99999);
-        add_image_size('fd-med', 768, 99999);
-        add_image_size('fd-sm', 320, 9999);
-
-        // rss thingy
-        add_theme_support('automatic-feed-links');
-
-        // Add post formats support. http://codex.wordpress.org/Post_Formats
-        add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
-
-        // Add menu support. http://codex.wordpress.org/Function_Reference/register_nav_menus
-        add_theme_support('menus');
-        register_nav_menus(array(
-            'primary' => __('Primary Navigation', 'reverie'),
-            'additional' => __('Additional Navigation', 'reverie'),
-            'utility' => __('Utility Navigation', 'reverie')
-        ));
-
-        // Add custom background support
-        add_theme_support( 'custom-background',
-            array(
-                'default-image' => '',  // background image default
-                'default-color' => '', // background color default (dont add the #)
-                'wp-head-callback' => '_custom_background_cb',
-                'admin-head-callback' => '',
-                'admin-preview-callback' => ''
-            )
-        );
-    }
-}
-add_action('after_setup_theme', 'reverie_theme_support'); /* end Reverie theme support */
-
-// create widget areas: sidebar, footer
-$sidebars = array('Sidebar');
-foreach ($sidebars as $sidebar) {
-    register_sidebar(array('name'=> $sidebar,
-    	'id' => 'Sidebar',
-        'before_widget' => '<article id="%1$s" class="panel widget %2$s">',
-        'after_widget' => '</article>',
-        'before_title' => '<h4>',
-        'after_title' => '</h4>'
-    ));
-}
-$sidebars = array('Footer');
-foreach ($sidebars as $sidebar) {
-    register_sidebar(array('name'=> $sidebar,
-    	'id' => 'Footer',
-        'before_widget' => '<div class="large-3 columns"><article id="%1$s" class="panel widget %2$s">',
-        'after_widget' => '</article></div>',
-        'before_title' => '<h4>',
-        'after_title' => '</h4>'
-    ));
-}
+/*
+7. lib/widget-areas.php
+    -let's add our widget areas
+*/
+require_once('lib/widget-areas.php');
 
 // return entry meta information for posts, used by multiple loops, you can override this function by defining them first in your child theme's functions.php file
 if ( ! function_exists( 'reverie_entry_meta' ) ) {
--- lib/custom-o-matic.php
+++ lib/custom-o-matic.php
@@ -1,4 +1,50 @@
 <?php 
+
+/**********************
+Add theme supports
+ **********************/
+if( ! function_exists( 'reverie_theme_support' ) ) {
+    function reverie_theme_support() {
+        // Add language supports.
+        load_theme_textdomain('reverie', get_template_directory() . '/lang');
+
+        // Add post thumbnail supports. http://codex.wordpress.org/Post_Thumbnails
+        add_theme_support('post-thumbnails');
+        // set_post_thumbnail_size(150, 150, false);
+        add_image_size('fd-lrg', 1024, 99999);
+        add_image_size('fd-med', 768, 99999);
+        add_image_size('fd-sm', 320, 9999);
+
+        // rss thingy
+        add_theme_support('automatic-feed-links');
+
+        // Add post formats support. http://codex.wordpress.org/Post_Formats
+        add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
+
+        // Add menu support. http://codex.wordpress.org/Function_Reference/register_nav_menus
+        add_theme_support('menus');
+        register_nav_menus(array(
+            'primary' => __('Primary Navigation', 'reverie'),
+            'additional' => __('Additional Navigation', 'reverie'),
+            'utility' => __('Utility Navigation', 'reverie')
+        ));
+
+        // Add custom background support
+        add_theme_support( 'custom-background',
+            array(
+                'default-image' => '',  // background image default
+                'default-color' => '', // background color default (dont add the #)
+                'wp-head-callback' => '_custom_background_cb',
+                'admin-head-callback' => '',
+                'admin-preview-callback' => ''
+            )
+        );
+    }
+}
+add_action('after_setup_theme', 'reverie_theme_support'); /* end Reverie theme support */
+
+//Add Our custom areas for Soleil
+
 function soleil_customize_register( $wp_customize ) {
     //Add Custom Header
     $wp_customize->add_section( 'soleil_logo_section' , array(
