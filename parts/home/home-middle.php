1ffd642b0a0e86cf75ec91fdfaa7e3b8cb407632
Luke Patrick <luke@snodandjeff.com>
2015-09-30 04:51:02 +0000

    Updates some middle styling and the favicon

--- parts/home/home-middle.php
+++ parts/home/home-middle.php
@@ -1,7 +1,8 @@
 <section class="home-middle">
     <div class="large-6 box left columns">
         <h4>smaller content</h4>
-        <h3>Test Content</h3>
+        <divider></divider>
+        <h2>Test Content</h2>
         <p>Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.</p>
         <a href="#" class="button">See More</a>
     </div>
