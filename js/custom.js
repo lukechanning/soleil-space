8e8b4028ebc0bc60fe5a62d638f634bbdc1e65f6
Luke Patrick <luke@snodandjeff.com>
2015-11-08 20:39:50 +0000

    Further refines booker agent, including adding sticky elements. Still not quite satisfied, thoughh

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,21 @@
   /////////////////////////////////
+ // Make sure booker is sticky //
+////////////////////////////////
+
+//Autofix_anything
+!function(e){var t={customOffset:false,manual:false,onlyInContainer:true};e.fn.autofix_anything=function(n){var r=e.extend({},t,n),i=e(this),s=i.position(),o=r.customOffset,u=i.offset();i.addClass("autofix_sb");e.fn.manualfix=function(){var t=e(this),n=t.offset();if(t.hasClass("fixed")){t.removeClass("fixed")}else{t.addClass("fixed").css({top:0,left:n.left,right:"auto",bottom:"auto"})}};fixAll=function(t,n,r,i){if(n.customOffset==false)o=t.parent().offset().top;if(e(document).scrollTop()>o&&e(document).scrollTop()<=t.parent().height()+(o-e(window).height())){t.removeClass("bottom").addClass("fixed").css({top:0,left:i.left,right:"auto",bottom:"auto"})}else{if(e(document).scrollTop()>o){if(n.onlyInContainer==true){if(e(document).scrollTop()>t.parent().height()-e(window).height()){t.addClass("bottom fixed").removeAttr("style").css({left:r.left})}else{t.removeClass("bottom fixed").removeAttr("style")}}}else{t.removeClass("bottom fixed").removeAttr("style")}}};if(r.manual==false){e(window).scroll(function(){fixAll(i,r,s,u)})}}}(window.jQuery)
+
+//And fire! 
+jQuery(document).ready(function() {
+    jQuery("#booker-wrap").autofix_anything({
+      customOffset: false, // You can define custom offset for when you want the container to be fixed. This option takes the number of pixels from the top of the page. The default value is false which the plugin will automatically fix the container when the it is in the viewport
+      manual: false, // Toggle this to true if you wish to manually fix containers with the public method. Default value is false
+      onlyInContainer: false // Set this to false if you don't want the fixed container to limit itself to the parent's container.
+    });
+});
+
+
+  /////////////////////////////////
  // Get our owl on right here  //
 ////////////////////////////////
 
@@ -58,8 +75,10 @@ jQuery(window).resize(function() {
 (function($) {
     $('#slide-out').click(function() {
         if ( $("#booker-agent").hasClass("clicked") ) {
-             $("#booker-agent").removeClass("clicked").animate({ "right" : "-=32rem" });
+             $('.book-content').hide();
+             $("#booker-agent").removeClass("clicked").animate({ "right" : "-=3rem" });
         } else {
+            $('.book-content').show();
             $("#booker-agent").addClass("clicked").animate({ "right" : "0" });
         }
     });
--- parts/booker.php
+++ parts/booker.php
@@ -1,13 +1,15 @@
-<div id="booker-agent" class="book">
-    <div id="slide-out" class="small-3 columns book-header">
-        <!-- Add a header -->
-        <h3 class="book-title">Book Now</h3>
-    </div>
-    <div class="small-9 columns book-content">
-        <?php
-            if ( ! dynamic_sidebar('booker') ) :
-                dynamic_sidebar('booker');
-            endif;
-        ?>
+<div id="booker-wrap">
+    <div id="booker-agent" class="book">
+        <div id="slide-out" class="small-3 columns book-header">
+            <!-- Add a header -->
+            <h3 class="book-title">Book Now</h3>
+        </div>
+        <div class="small-9 columns book-content">
+            <?php
+                if ( ! dynamic_sidebar('booker') ) :
+                    dynamic_sidebar('booker');
+                endif;
+            ?>
+        </div>
     </div>
 </div>
